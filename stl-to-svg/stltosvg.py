3cb2375f08a9a34bf99af27c91a7d67eb81e9f96
Jeremy Reeve <jeremy.reeve81@gmail.com>
2016-08-27 18:51:41 +0100

    Converter to extract interesting layers from STL meshes and turn them in to SVG with units applied to allow import in to Inkscape for example.  This lets one take some stuff from an openscad modelm output as STL and turn it in to input suitable for the laser cutter.  It avoid multiple steps exporting DXF, importing DXF and converting to SVG which has idiosyncrasies all of its own.  ****WIP**** not production ready yet.

---
+++ stl-to-svg/stltosvg.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+#
+# @(#)stltosvg.py
+#
+# Copyright (c) 2016 Jeremy Reeve
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""stltosvg.py
+
+This module allows one to provide an STL mesh file, process it as though
+it were being sliced for 3D printing, select which of the pertinent layers
+one is interested in and have the resultant SVG document post-processed
+to ensure correct units are used on import by a vector graphics program
+such as Inkscape. 
+
+Example:
+
+        $ python stltosvg.py --layer 3 mymodel.stl
+#
+# If no layer is specified then all pertinent layers are output to 
+# separate files.
+# 
+#
+"""
+
+import argparse
+import subprocess
+import os.path
+
+from lxml import etree
+
+INTERMEDIATE_FILE_SUFFIX = 'all'
+FILETYPE_SUFFIX = 'svg'
+XML_NAMESPACE_SVG = {'svg' : 'http://www.w3.org/2000/svg'}
+
+def main():
+    parser = argparse.ArgumentParser(description='Convert STL to SVG document '
+                                     'containing a single slice or layer of the'
+                                     ' mesh.',
+                                     epilog='''Example:
+
+        $ python stltosvg.py --layer 3 mymodel.stl
+''')
+    parser.add_argument('inputfile', nargs=1,
+                        help='input file')
+    parser.add_argument('--layer', type=int, nargs='*', action='append',
+                        help='which layer(s) to process')
+    parser.add_argument('--version', action='version', version='%(prog)s 0.1')
+
+    args = parser.parse_args()
+
+    path, filename = os.path.split(args.inputfile[0])
+    filenameroot, _ = os.path.splitext(filename)
+
+    print(path, filename)
+    
+    OUTPUT_FILENAME_FORMAT = ''.join(['[input_filename_base].',
+                                      INTERMEDIATE_FILE_SUFFIX, '.',
+                                      FILETYPE_SUFFIX])
+
+    # The following is a bit dokey but necessary
+    SVG_FILENAME_SLICED = os.path.join(path,
+                                       ''.join([filenameroot,
+                                                '.',
+                                                INTERMEDIATE_FILE_SUFFIX,
+                                                '.',
+                                                FILETYPE_SUFFIX]))
+    
+    try:
+        completed = subprocess.run(['slic3r', '--output-filename-format',
+                                    OUTPUT_FILENAME_FORMAT,
+                                    '--export-svg', args.inputfile[0]],
+                                   check=True)
+    except subprocess.CalledProcessError as err:
+        print("Problem slicing: ", str(err))
+        sys.exit(1)
+    
+    try:
+        with open(SLICED_SVG_FILENAME) as fp:
+            all_layers_tree = etree.parse(fp)
+            root_svg = all_layers_tree.getroot()
+            layers = all_layers_tree.xpath('/svg:svg/svg:g',
+                                           namespaces=XML_NAMESPACE_SVG)
+            
+
+            for layer_group in layers:
+                layer_id = layer_group.attrib['id']
+                print('Processing: ', layer_id)
+
+                layer_tree = etree.ElementTree()
+                layer_tree.docinfo = all_layers_tree.docinfo
+
+                svg_element = layer_tree.makeelement('svg')
+                svg_element.attrib = root_svg.attrib
+                width = int(svg_element.attrib['width'])
+                height = int(svg_element.attrib['height'])
+                svg_element.attrib['width'].append('mm')
+                svg_element.attrib['height'].append('mm')
+
+                svg_element.set('viewBox', ''.join(['0 0 ', width, ' ',
+                                                    height]))
+
+                svg_element.append(layer_group)
+                layer_tree.append(svg_element)
+                #create filename for individual layer file:
+                SVG_FILENAME_LAYER = os.path.join(path,
+                                                  ''.join([filenameroot,
+                                                           '.',
+                                                           layer_id,
+                                                           '.',
+                                                           FILETYPE_SUFFIX]))
+    
+                with open(SVG_FILENAME_LAYER) as lf:
+                    layer_tree.write(lf)
+
+                
+    except EnvironmentError as err:
+        print("Oh dear! ", str(err))
+        sys.exit(1)
+
+
+
+    
+
+if __name__ == '__main__':
+    main()
